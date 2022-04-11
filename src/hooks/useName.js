81a771df2bb3021f29dbc41218ada2e083fa3ec8
sycue <sycue@horde.network>
2022-04-11 17:23:09 +0800

    remove gas params

--- src/hooks/useName.js
+++ src/hooks/useName.js
@@ -143,8 +143,8 @@ export const useConnect = () => {
 export const sendTransaction = async () => {
   const transactionParameters = {
     nonce: "0x00", // ignored by MetaMask
-    gasPrice: "0x09184e72a000", // customizable by user during MetaMask confirmation.
-    gas: "0x2710", // customizable by user during MetaMask confirmation.
+    // gasPrice: "0x09184e72a000", // customizable by user during MetaMask confirmation.
+    // gas: "0x2710", // customizable by user during MetaMask confirmation.
     to: "0x0000000000000000000000000000000000000000", // Required except during contract publications.
     from: window.ethereum.selectedAddress, // must match user's active address.
     value: "0x00", // Only required to send ether to the recipient from the initiating external account.
